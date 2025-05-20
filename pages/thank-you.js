d518312465b8a541d6dce382db8f9edb2315e7a3
matthiasvanlent <matthiasvanlent@hotmail.com>
2025-05-20 13:34:03 +0200

    Create thank-you.js

---
+++ pages/thank-you.js
@@ -0,0 +1,8 @@
+export default function ThankYou() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🎉 You're confirmed!</h1>
+      <p>Thanks for confirming your participation.</p>
+    </div>
+  );
+}
